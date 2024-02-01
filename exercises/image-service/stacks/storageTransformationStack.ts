a146fc5af2f26ffb1bb99cfa99410da5f538f2d8
Joel S. Roxell <joel.roxell@elva-group.com>
2024-02-01 16:46:16 +0100

    fix(project): add img service excerise

---
+++ exercises/image-service/stacks/storageTransformationStack.ts
@@ -0,0 +1,7 @@
+import { StackContext, Bucket, Queue, EventBus } from 'sst/constructs';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import * as events from 'aws-cdk-lib/aws-events';
+
+export function ImageTransformationStack({ stack }: StackContext) {
+  return {};
+}
